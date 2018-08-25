8cbbdb83c1109833cce86c93f6ca896f2fddd7fe
Raghu <raghu.teegela@gmail.com>
2018-08-25 15:31:22 +0530

    Add call pojo

---
+++ app/src/main/java/com/direct/whatsapp/rgu/whatsappdirect/data/CallLogData.kt
@@ -0,0 +1,14 @@
+package com.direct.whatsapp.rgu.whatsappdirect.data
+
+import java.util.*
+
+class CallLogData {
+
+    var phoneNumber : String? = null
+    var callType : String? = null
+    var date : String? = null
+    var dayTime : Date? = Date()
+    var duration : String? = null
+    var dirCode : String? = null
+
+}
